595a064080bc9f233cc1b272496909e000dfca67
encomiastical <makeablub@gmail.com>
2023-12-04 18:44:21 +0100

    Update setup_snd-hda-scodec-cs35l41.sh

--- setup_snd-hda-scodec-cs35l41.sh
+++ setup_snd-hda-scodec-cs35l41.sh
@@ -16,19 +16,43 @@ DKMS_MODULE_VERSION='0.1'
 
 # create the patch file to apply to the source of the snd-hda-scodec-cs35l41 kernel module
 sudo tee "/usr/src/${KERNEL_MODULE_NAME}-${DKMS_MODULE_VERSION}/cs35l41_hda.patch" <<'EOF'
---- sound/pci/hda/cs35l41_hda.c
-+++ sound/pci/hda/cs35l41_hda.c
-@@ -1235,6 +1235,10 @@
- 		hw_cfg->bst_type = CS35L41_EXT_BOOST;
- 		hw_cfg->gpio1.func = CS35l41_VSPK_SWITCH;
- 		hw_cfg->gpio1.valid = true;
-+  } else if (strncmp(hid, "CSC3551", 7) == 0) {
-+     hw_cfg->bst_type = CS35L41_EXT_BOOST;
-+     hw_cfg->gpio1.func = CS35l41_VSPK_SWITCH;
-+     hw_cfg->gpio1.valid = true;
- 	} else {
- 		/*
- 		 * Note: CLSA010(0/1) are special cases which use a slightly different design.
+*** cs35l41_hda_property.c	2023-12-04 18:17:02.482381486 +0100
+--- cs35l41_hda_property_patch.c	2023-12-04 18:23:02.208476883 +0100
+***************
+*** 34,40 ****
+  
+  	if (strcmp(hid, "CLSA0100") == 0) {
+  		hw_cfg->bst_type = CS35L41_EXT_BOOST_NO_VSPK_SWITCH;
+! 	} else if (strcmp(hid, "CLSA0101") == 0) {
+  		hw_cfg->bst_type = CS35L41_EXT_BOOST;
+  		hw_cfg->gpio1.func = CS35l41_VSPK_SWITCH;
+  		hw_cfg->gpio1.valid = true;
+--- 34,40 ----
+  
+  	if (strcmp(hid, "CLSA0100") == 0) {
+  		hw_cfg->bst_type = CS35L41_EXT_BOOST_NO_VSPK_SWITCH;
+! 	} else if (strcmp(hid, "CLSA0101") == 0 || strcmp(hid, "CSC3551") == 0) {
+  		hw_cfg->bst_type = CS35L41_EXT_BOOST;
+  		hw_cfg->gpio1.func = CS35l41_VSPK_SWITCH;
+  		hw_cfg->gpio1.valid = true;
+***************
+*** 91,97 ****
+  static const struct cs35l41_prop_model cs35l41_prop_model_table[] = {
+  	{ "CLSA0100", NULL, lenovo_legion_no_acpi },
+  	{ "CLSA0101", NULL, lenovo_legion_no_acpi },
+! 	{ "CSC3551", "103C89C6", hp_vision_acpi_fix },
+  	{}
+  };
+  
+--- 91,98 ----
+  static const struct cs35l41_prop_model cs35l41_prop_model_table[] = {
+  	{ "CLSA0100", NULL, lenovo_legion_no_acpi },
+  	{ "CLSA0101", NULL, lenovo_legion_no_acpi },
+! 	{ "CSC3551", NULL, lenovo_legion_no_acpi },
+! 	{ "CSC3551", "103C89C6", lenovo_legion_no_acpi },
+  	{}
+  };
+
 EOF
 
 clear
